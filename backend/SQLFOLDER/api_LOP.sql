94cb2dab8a5e721684b7cec90dccf96115ae5757
Jho <jowa.kaizen16@gmail.com>
2025-06-16 00:11:01 +0800

    Added Backend Fetching

---
+++ backend/SQLFOLDER/api_LOP.sql
@@ -0,0 +1,61 @@
+
+      {
+        "expenseTitle": "Travelling Expense"
+      },
+      {
+        "expenseTitle": "Internet Expense (Pocket Wifi)"
+      },
+      {
+        "expenseTitle": "Training Expense"
+      },
+      {
+        "expenseTitle": "Office/Other Supplies Expense (DBM)"
+      },
+      {
+        "expenseTitle": "Training Expenses (GAD/INSET)"
+      },
+      {
+        "expenseTitle": "Office/Other Supplies Expense (Outside DBM)"
+      },
+      {
+        "expenseTitle": "Training Expenses (LAC Session)"
+      },
+      {
+        "expenseTitle": "Fidelity Bond Premium"
+      },
+      {
+        "expenseTitle": "Drugs and Medicines / Medical / Dental / Lab Supplies"
+      },
+      {
+        "expenseTitle": "Electricity Expense"
+      },
+      {
+        "expenseTitle": "Water Expense"
+      },
+      {
+        "expenseTitle": "Mobile Expense"
+      },
+      {
+        "expenseTitle": "Internet Expense (DSL/Globe)"
+      },
+      {
+        "expenseTitle": "Security Services (Tanod/Non-Agency)"
+      },
+      {
+        "expenseTitle": "Food Supplies Expense (Feeding)"
+      },
+      {
+        "expenseTitle": "Security Services (Agency)"
+      },
+      {
+        "expenseTitle": "Labor and Wages"
+      },
+      {
+        "expenseTitle": "Janitorial Services (Non-Agency)"
+      },
+      {
+        "expenseTitle": "Representation Expense"
+      },
+      {
+        "expenseTitle": "Fuel, Oil, and Lubricants Expense"
+        },
