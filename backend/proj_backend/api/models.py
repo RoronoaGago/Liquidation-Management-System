09cbf305de4a62489e184cf31a8c791e27236c7e
Harvey <htaningco@gmail.com>
2025-06-22 16:43:45 +0800

    enhance the notauthorized page and reset my database again and again

--- backend/proj_backend/api/models.py
+++ backend/proj_backend/api/models.py
@@ -22,11 +22,6 @@ class User(AbstractUser):
         ('accountant', 'Division Accountant'),
     ]
 
-    SEX_CHOICES = [
-        ('male', 'Male'),
-        ('female', 'Female'),
-    ]
-
     role = models.CharField(
         max_length=20,
         choices=ROLE_CHOICES,
@@ -40,8 +35,6 @@ class User(AbstractUser):
         related_name='users'
     )
     date_of_birth = models.DateField(null=True, blank=True)
-    sex = models.CharField(
-        max_length=10, choices=SEX_CHOICES, null=True, blank=True)
     phone_number = models.CharField(max_length=20, blank=True, null=True)
     profile_picture = models.ImageField(
         upload_to='profile_pictures/',
