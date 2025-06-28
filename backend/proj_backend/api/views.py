422e5f739826293548071fb53623587bc61d5660
Jho <jowa.kaizen16@gmail.com>
2025-06-28 21:11:25 +0800

    Allowed bulk posting in school view

--- backend/proj_backend/api/views.py
+++ backend/proj_backend/api/views.py
@@ -199,6 +199,16 @@ class SchoolListCreateAPIView(generics.ListCreateAPIView):
             )
         return queryset.order_by('schoolName')
 
+    def create(self, request, *args, **kwargs):
+        # Check if request.data is a list (batch)
+        is_many = isinstance(request.data, list)
+        serializer = self.get_serializer(data=request.data, many=is_many)
+        serializer.is_valid(raise_exception=True)
+        self.perform_create(serializer)
+        headers = self.get_success_headers(serializer.data)
+        return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
+
+
 # Add a new endpoint for school search
 
 
