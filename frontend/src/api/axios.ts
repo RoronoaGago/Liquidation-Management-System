7dc2034e79b9eea27368be71bb8d060496600845
Jho <jowa.kaizen16@gmail.com>
2025-08-25 21:46:05 +0800

    Changed URL to IP Address

--- frontend/src/api/axios.ts
+++ frontend/src/api/axios.ts
@@ -7,7 +7,9 @@ interface TokenResponse {
 }
 
 const api = axios.create({
-  baseURL: "http://127.0.0.1:8000/api/",
+  // baseURL: "http://127.0.0.1:8000/api/",
+  baseURL: "http://192.168.1.91:8000/api/",
+
   headers: {
     "Content-Type": "application/json",
   },
