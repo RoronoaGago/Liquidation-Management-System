e8dec9762cd2d7c24787b51a5517fad16c58cd07
Jho <jowa.kaizen16@gmail.com>
2025-10-11 20:58:36 +0800

    creating superuser

--- backend/proj_backend/api/models.py
+++ backend/proj_backend/api/models.py
@@ -29,6 +29,7 @@ class UserManager(BaseUserManager):
     def create_superuser(self, email, password=None, **extra_fields):
         extra_fields.setdefault('is_staff', True)
         extra_fields.setdefault('is_superuser', True)
+        extra_fields.setdefault('role', 'admin')  # Set role to admin for superusers
         return self.create_user(email, password, **extra_fields)
 
 
